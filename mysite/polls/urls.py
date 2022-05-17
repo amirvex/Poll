c9eeee099f062877f696f89469101a7f079bd7b6
amirvex <amir.amani51@gmail.com>
2022-05-17 11:09:23 +0430

    added poll route

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-
+from django.urls import include, path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+]
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls)
 ]
