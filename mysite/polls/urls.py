c0fe2ecd1b7f115007bacab0dbb850d95a7c7bd7
amirvex <amir.amani51@gmail.com>
2022-05-17 11:25:59 +0430

    bug fixed

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,12 +1,7 @@
-from django.urls import include, path
-from django.contrib import admin
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-]
-
-urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls)
+    path('', views.index, name='index')
 ]
