69f78ecb7495f9fac840d6b726294e820b6e079f
amirvex <amir.amani51@gmail.com>
2022-05-18 21:42:32 +0430

    added data

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,28 @@
+from multiprocessing.spawn import import_main_path
 from re import template
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import Members
 
 
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+
+def index(request):
+    mymembers = Members.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output += x["firstname"]
+    return HttpResponse(output)
+
+
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
