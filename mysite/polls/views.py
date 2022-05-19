0a9dfefab6d1f45af4ebbdda6ea09bb200229aa3
amirvex <amir.amani51@gmail.com>
2022-05-19 11:52:27 +0430

    delete button added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,8 @@ from multiprocessing.spawn import import_main_path
 from re import template
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Members
 
 
@@ -26,3 +27,22 @@ def index(request):
         'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
